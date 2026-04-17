46f8d669bf243d040e0c5223bee9b6b8787a55df
kazimizan77 <kazimizan77@yahoo.com>
2026-04-17 10:34:44 +0600

    fix: make footer section pixel-perfect

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -6,40 +6,29 @@ import x from "../../assets/images/twitter.png";
 const Footer = () => {
   return (
     <footer className="bg-[#244d3fFF] text-white">
-      <div className="max-w-[1600px] mx-auto px-4 md:px-6 py-16 text-center">
-        <img src={logo} alt="footer logo" className="h-[40px] mx-auto mb-4" />
+      <div className="max-w-[1280px] mx-auto px-4 md:px-6 pt-10 md:pt-20 pb-4 md:pb-8 text-center">
+        <img src={logo} alt="footer logo" className="h-10 mx-auto mb-4" />
 
-        <p className="text-white/80 mb-6">
-          Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
+        <p className="text-white/80 mb-6 text-sm xl:text-base">
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
         </p>
 
         <p className="text-white text-[20px] font-medium mb-4">Social Links</p>
 
         <div className="flex items-center justify-center gap-3 mb-10">
           <span className="w-10 h-10 rounded-full bg-white flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-            <img
-              src={instagram}
-              alt="Instagram"
-              className="w-10 h-10 object-contain"
-            />
+            <img src={instagram} alt="Instagram" />
           </span>
           <span className="w-10 h-10 rounded-full bg-white flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-            <img
-              src={facebook}
-              alt="Facebook"
-              className="w-10 h-10 object-contain"
-            />
+            <img src={facebook} alt="Facebook" />
           </span>
           <span className="w-10 h-10 rounded-full bg-white flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-            <img
-              src={x}
-              alt="x"
-              className="w-10 h-10 object-contain"
-            />
+            <img src={x} alt="x" />
           </span>
         </div>
 
-        <div className="border-t border-white/10 pt-8 flex flex-col md:flex-row items-center justify-between gap-4 text-white/70">
+        <div className="border-t border-emerald-500/15 pt-7 flex flex-col md:flex-row items-center justify-between gap-4 text-white/50">
           <p> &copy; 2026 KeenKeeper. All rights reserved.</p>
           <div className="flex items-center gap-6">
             <span className="cursor-pointer hover:text-white transition">
